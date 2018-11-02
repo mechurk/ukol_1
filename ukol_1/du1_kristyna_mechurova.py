cb991ccd6f2d07c6fc143c0ca76b0e61a54eca51
mechurk <londynska56>
2018-11-03 00:18:38 +0100

    uprava_2

--- ukol_1/du1_kristyna_mechurova.py
+++ ukol_1/du1_kristyna_mechurova.py
@@ -5,76 +5,93 @@ import math
 #osetreni polomeru
 def zadani_osetreni_polomeru():
     while True:
-        polomerstr = input("zadej polomer v km:")
-        polomer = float(polomerstr)
-        if polomer <0:
-            print("chybne zadany polomer")
-        elif polomer ==0:
-            return 6371.11
+        polomerstr = input("zadej R v km:")
+        R = float(polomerstr)
+        if R <0:
+            print("chybne zadany R")
+        elif R ==0:
+            return 6371.11*10000
         else:
-            return polomer
+            return R*10000
 
-polomer = zadani_osetreni_polomeru()
+
 
 #osetreni meritka
-while True:
-    meritkostr = input("zadej meritko:")
-    meritko = int(meritkostr)
-    if meritko <=0:
-        print("chybne zadane meritko")
-    else:
-        break
+def zadani_osetreni_meritka():
+    while True:
+        meritkostr = input("zadej meritko:")
+        try:
+            m = int(meritkostr)
+            if m <=0:
+                print("chybne zadane meritko - zadejte kladne cislo")
+            else:
+                m = m / 10
+                return m
+        except:
+            print("chybne zadane meritko - zadejte cele cislo")
 
 
-m=meritko/10
-R=polomer*10000
-#vytvoreni listu stupnu polednikove a rovnikove site
-u = [i for i in range(-90,100,10)]
-v = [i for i in range(-180,190,10)]
-#vytvoreni prazdneho listu do ktereho se budou zapisovat vypocty
-poledniky=[]
-rovnobezky=[]
 
 #osetreni zadani pismene pro zobrazeni
-while True:
-    zobrazeni = input("zadej pismeno A,B,M nebo L: ")
-    if zobrazeni == "A" or zobrazeni == "B" or zobrazeni == "M" or zobrazeni == "L":
-        break
-    else:
-        print("nespravne zadane pismeno")
+def zadani_osetreni_pismeneproZobr():
+    while True:
+        zobrazeni = input("zadej pismeno A,B,M nebo L: ")
+        if zobrazeni == "A" or zobrazeni == "B" or zobrazeni == "M" or zobrazeni == "L":
+            return zobrazeni
+        else:
+            print("nespravne zadane pismeno")
+
+
 
 #vypocet poledniku
-for i in v:
-    x=(R*i*math.pi/180)/m
-    x=round(x,1)
-    if -100 >= x or x >=100:
-        x="-"
-    poledniky.append(x)
+
+def vypocet_poledniku (R,m):
+    v = [i for i in range(-180, 190, 10)]
+    poledniky = []
+    for i in v:
+        x=(R*i*math.pi/180)/m
+        x=round(x,1)
+        #osetreni pokud je hodnota vyssi nez jeden metr
+        if -100 >= x or x >=100:
+            x="-"
+        poledniky.append(x)
+    return poledniky
+
+
 
 #vypocet rovnobezek
-for i in u:
+def vypocet_rovnobezek(R,m,zobrazeni):
+    u = [i for i in range(-90, 100, 10)]
+    rovnobezky = []
+    v = [i for i in range(-180,190,10)]
+    for i in u:
 
-    if zobrazeni=="L":
-        y=(R*math.sin(i*math.pi/180))/m
-    elif zobrazeni=="A":
-        y=(R*i*math.pi/180)/m
-    elif zobrazeni=="B":
-        y=(2*R*math.tan(i/2*math.pi/180))/m
-    elif zobrazeni=="M":
-        if abs(i)==90:
-            y='-' #pol se nezobrazuje
-        else:
-            y=(R*math.log(math.tan(((i/2)+45)* math.pi / 180)))/m
-    #else:
-       # print ("Chybne zadane pismeno")
-       # break
+        if zobrazeni=="L":
+            y=(R*math.sin(i*math.pi/180))/m
+        elif zobrazeni=="A":
+            y=(R*i*math.pi/180)/m
+        elif zobrazeni=="B":
+            y=(2*R*math.tan(i/2*math.pi/180))/m
+        elif zobrazeni=="M":
+            if abs(i)==90:
+                y='-' #pol se nezobrazuje
+            else:
+                y=(R*math.log(math.tan(((i/2)+45)* math.pi / 180)))/m
 
 
-    #osetreni pokud je hodnota vetsi nez jeden metr
-    y=round(y,1)
-    if -100 >= y or y >=100:
-        y="-"
-    rovnobezky.append(y)
+        #osetreni pokud je hodnota vetsi nez jeden metr
+        y=round(y,1)
+        if -100 >= y or y >=100:
+            y="-"
+
+        rovnobezky.append(y)
+    return rovnobezky
+
+R = zadani_osetreni_polomeru()
+m=zadani_osetreni_meritka()
+zobrazeni=zadani_osetreni_pismeneproZobr()
+poledniky=vypocet_poledniku (R,m)
+rovnobezky=vypocet_rovnobezek(R,m,zobrazeni)
 
 #vypsani poledniku a rovnobezek
 print ("Rovnobezky:",rovnobezky)
