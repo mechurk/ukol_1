96b41981ec721b38d822bc52d41bfea481a3385d
mechurk <londynska56>
2018-12-14 16:42:22 +0100

    update2

--- ukol_2/du2_kristyna_mechurova.py
+++ ukol_2/du2_kristyna_mechurova.py
@@ -15,42 +15,67 @@ for polozky in gj ['features']:
 print (body)
 
 
-#def calc_bbox(body):
-minx = float("inf")
-miny = float("inf")
-maxx = float("-inf")
-maxy = float("-inf")
-for bod in body:
-    if bod [1] < minx:
-        minx = bod [1]
-    if bod [1]> maxx:
-        maxx = bod [1]
-    if bod [2]< miny:
-        miny = bod [2]
-    if bod [2] > maxy:
-        maxy = bod [2]
-bbox= [minx, miny,maxx, maxy]
-print (minx,miny,maxx,maxy)
-#return (bbox)
-
-
+def calc_bbox(body):
+    minx = float("inf")
+    miny = float("inf")
+    maxx = float("-inf")
+    maxy = float("-inf")
+    for bod in body:
+        if bod [1] < minx:
+            minx = bod [1]
+        if bod [1]> maxx:
+            maxx = bod [1]
+        if bod [2]< miny:
+            miny = bod [2]
+        if bod [2] > maxy:
+            maxy = bod [2]
+    bbox= [minx, miny,maxx, maxy]
+    print (minx,miny,maxx,maxy)
+    return (bbox)
 
 #bbox=calc_bbox(body)
-print(bbox)
 
-strx=((bbox[2]-bbox[0])/2)+bbox[0]
-stry=((bbox[3]-bbox[1])/2)+bbox[1]
-print (strx)
-print (stry)
-for bod in body:
-    if bod [1] < strx and bod[2]> stry:
-        ctvrtina =1
-    elif bod [1] > strx and bod[2]> stry:
-        ctvrtina =2
-    elif bod [1] < strx and bod[2]< stry:
-        ctvrtina =3
-    elif bod[1] > strx and bod[2] < stry:
-        ctvrtina = 4
-    bod.append(ctvrtina)
 
+
+
+def processQuarter(body,box,cluster_id):
+    pointsInThisBox = pointsInBox(body,box)
+    print(pointsInThisBox)
+    if (len(pointsInThisBox) < 50):
+        for point in pointsInThisBox:
+            point.append(int(cluster_id))
+        return
+    else :
+        processQuarter(body,topLeft(box),cluster_id+"1")
+        processQuarter(body,topRight(box),cluster_id+"2")
+        processQuarter(body,bottomLeft(box),cluster_id+"3")
+        processQuarter(body,bottomRight(box),cluster_id+"4")
+
+
+
+def pointsInBox(body,bbox):
+    return [point for point in body if bbox[0] <= point [1] < bbox[2] and bbox[1] <= point[2] < bbox[3] ]
+
+def topLeft(box):
+    stred = [(box[2] + box[0]) / 2, (box[3] + box[1]) / 2]
+    qbox=[box[0], stred[1], stred[0], box[3]]
+    return qbox
+def topRight(box):
+    stred = [(box[2] + box[0]) / 2, (box[3] + box[1]) / 2]
+    qbox=[stred[0], stred[1], box[2], box[3]]
+    return qbox
+def bottomLeft(box):
+    stred = [(box[2] + box[0]) / 2, (box[3] + box[1]) / 2]
+    qbox=[box[0], box[1], stred[0], stred[1]]
+    return qbox
+def bottomRight(box):
+    stred = [(box[2] + box[0]) / 2, (box[3] + box[1]) / 2]
+    qbox=[stred[0], box[1],box[2], stred[1]]
+    return qbox
+
+
+box=calc_bbox(body)
+processQuarter(body,box,"")
 print (body)
+
+
